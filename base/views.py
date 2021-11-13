575c95bf78181c66b0c0eabb54cf756df9ac8198
Ka Wing Tsang <kawingt2@gmail.com>
2021-11-13 10:54:16 +0800

    build Message model and display model in Home

--- base/views.py
+++ base/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render
+from .models import Room
 
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Lets learn react!'},
-    {'id': 3, 'name': 'Frontend dev'}
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Lets learn react!'},
+#     {'id': 3, 'name': 'Frontend dev'}
+# ]
 
 # Funation base view
 
 
 def home(request):
-    context = {'rooms': rooms} #context dictionary
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}   #context dictionary
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context= {'room': room}
     return render(request, 'base/room.html', context)
