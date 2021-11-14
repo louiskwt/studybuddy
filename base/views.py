6f98e8f71a3d1b58e87dc451c04b9327b54aff7e
Ka Wing Tsang <kawingt2@gmail.com>
2021-11-15 00:38:42 +0800

    add sidebar filter

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
@@ -15,8 +15,12 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}   #context dictionary
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    
+    topics = Topic.objects.all()
+    
+    context = {'rooms': rooms, 'topics': topics}   #context dictionary
     return render(request, 'base/home.html', context)
 
 
