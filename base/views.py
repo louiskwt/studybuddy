7559e34df5fa151f0c544ccd753a3cf78e669453
Ka Wing Tsang <kawingt2@gmail.com>
2021-11-16 15:12:47 +0800

    in login view, redirect user to home if logged in

--- base/views.py
+++ base/views.py
@@ -21,6 +21,9 @@ from django.contrib import messages
 
 def loginPage(request):
     
+    if request.user.is_authenticated:
+        return redirect('home')
+    
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
