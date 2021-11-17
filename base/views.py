d9fd23a8b0691173337dec134cb27e78b58b7b4e
Ka Wing Tsang <kawingt2@gmail.com>
2021-11-17 12:14:17 +0800

    add participants to model and display participants in room

--- base/views.py
+++ base/views.py
@@ -88,7 +88,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
-    
+    participants = room.participants.all()
     if request.method == "POST":
         message = Message.objects.create(
             user=request.user,
@@ -97,7 +97,7 @@ def room(request, pk):
         )
         return redirect('room', pk=room.id) #reload the page
     
-    context= {'room': room, 'room_messages': room_messages}
+    context= {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
